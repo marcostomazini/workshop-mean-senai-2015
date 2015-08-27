c44572484eeb1433fd6f2bd4027ce1cef20e3857
marcos.tomazini@gmail.com <marcos.tomazini@gmail.com>
2015-08-27 10:07:15 +0000

    enviando responde pelo end

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# workshop-mean-senai-2015
+# Workshop
 Workshop MEAN com Suissa Senai 2015
+
+# Console Colorido para o Mongo
+https://tylerbrock.github.io/mongo-hacker/
+
+# Http Status Code
+https://www.flickr.com/photos/girliemac/sets/72157628409467125/
+http://httpstatusdogs.com/
--- node/hello-world.js
+++ node/hello-world.js
@@ -3,7 +3,8 @@ var http = require("http");
 http.createServer(function(request, response) {
 response.writeHead(200, 
 {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+  // response.write("Hello World");
+  // response.end();
+  response.end("Hello World");
 }).listen(3000);
 console.log('Entre em http://localhost:3000/');
